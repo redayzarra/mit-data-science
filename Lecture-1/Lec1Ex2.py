685e9deb5a0d67f28c92f5d94c3f8e41456fe336
ReDay Zarra <redayzarra@gmail.com>
2022-11-16 17:06:43 -0500

    Adding all final notes for Lecture 1

--- Lecture-1/Lec1Ex2.py
+++ Lecture-1/Lec1Ex2.py
@@ -1,1 +1,43 @@
 # UNIT 1 - Lecture 1: Excercise 2
+
+# Please help the burglar out! For each of the following greedy metrics, what should be the burglar's first two choices of items? Here's a table of the items from the slides:
+ 
+"item"    |	"$" |"kg"|"$/kg"
+"clock"	  | 175 |	10 | 17.5
+"picture"	| 90	| 9	 | 10
+"radio"	  | 20	| 4	 | 5
+"vase"	  | 50	| 2	 | 25
+"book"	  | 10	| 1	 | 10
+"computer"| 200	| 20 | 10
+
+# For this problem, assume that the maximum weight the burglar can carry is 20.
+
+################################################################################
+
+# 1. Metric: max value
+
+# The burglar should first pick:
+# computer [X] Correct.
+
+# and should next pick:
+# no more space [X] Correct.
+
+################################################################################
+
+# 2. Metric: min  weight
+
+# The burglar should first pick:
+# book [X] Correct.
+
+# and should next pick:
+# vase [X] Correct.
+
+################################################################################
+
+# 3. Metric: max value/weight ratio
+
+# The burglar should first pick:
+# vase [X] Correct.
+
+# and should next pick:
+# clock [X] Correct.
